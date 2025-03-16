950c003cea06407f31196a28c3299a7627e9c253
TheWire <TheWire@users.noreply.github.com>
2025-03-16 02:53:51 +0000

    posts in feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import PostEditor from "@/components/posts/editor/PostEditor";
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+import Post from "@/components/posts/editor/Post";
+import { postDataInclude, type PostData } from "@/lib/types";
+
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full"><PostEditor /></div>
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/components/posts/editor/Post.tsx
@@ -0,0 +1,37 @@
+import UserAvatar from "@/components/UserAvatar";
+import { PostData } from "@/lib/types";
+import { formatRelativeDate } from "@/lib/utils";
+import Link from "next/link";
+
+interface PostProps {
+  post: PostData;
+}
+export default function Post({ post }: PostProps) {
+  return (
+    <article className="round-2xl space-y-3 bg-card p-5 shadow-sm">
+      <div className="flex flex-wrap gap-3">
+        <Link
+          href={`/users/${post.user.username}`}
+          className="flex items-center gap-2"
+        >
+          <UserAvatar avatarUrl={post.user.avatarUrl} />
+        </Link>
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          >
+            {formatRelativeDate(post.createdAt)}
+          </Link>
+        </div>
+      </div>
+      <div className="whitespace-pre-line break-words">{post.content}</div>
+    </article>
+  );
+}
