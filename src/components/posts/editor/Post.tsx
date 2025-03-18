5e7cd7b2dc15a250da24859dfc227e71c2ce6519
TheWire <TheWire@users.noreply.github.com>
2025-03-18 02:33:17 +0000

    infinite scroll + post appear immediately

--- src/components/posts/editor/Post.tsx
+++ src/components/posts/editor/Post.tsx
@@ -8,7 +8,7 @@ interface PostProps {
 }
 export default function Post({ post }: PostProps) {
   return (
-    <article className="rounded-2xl space-y-3 bg-card p-5 shadow-sm">
+    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex flex-wrap gap-3">
         <Link
           href={`/users/${post.user.username}`}
@@ -27,7 +27,7 @@ export default function Post({ post }: PostProps) {
             href={`/posts/${post.id}`}
             className="block text-sm text-muted-foreground hover:underline"
           >
-            {/* {formatRelativeDate(post.createdAt)} */}
+            {formatRelativeDate(post.createdAt)}
           </Link>
         </div>
       </div>
