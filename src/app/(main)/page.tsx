971ea3fa7aaf49f8b36e7864e9081fa9b2165aa6
TheWire <andrew.ab@gmail.com>
2025-03-25 01:14:33 +0000

    use profile page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,4 @@
-import Image from "next/image";
 import PostEditor from "@/components/posts/editor/PostEditor";
-import { prisma } from "@/lib/prisma";
-import Post from "@/components/posts/Post";
-import { postDataInclude, type PostData } from "@/lib/types";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import ForYouFeed from "./ForYouFeed";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
