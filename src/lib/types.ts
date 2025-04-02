0d20b69a7516ccb58ae498e39a4bc0c33ead84c9
TheWire <TheWire@users.noreply.github.com>
2025-04-02 15:50:03 +0100

    bookmarks

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -43,6 +43,14 @@ export function getPostDataInclude(loggedInUseId: string) {
         userId: true,
       },
     },
+    bookmarks: {
+      where: {
+        userId: loggedInUseId,
+      },
+      select: {
+        userId: true,
+      },
+    },
     _count: {
       select: {
         likes: true,
@@ -68,4 +76,7 @@ export interface FollowerInfo {
 export interface LikeInfo {
   likes: number;
   isLikedByUser: boolean;
+}
+export interface BookmarkInfo {
+  isBookmarkedByUser: boolean;
 }
