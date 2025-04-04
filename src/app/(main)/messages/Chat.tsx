919cb4f416bea04adc7cc92bcb8cfb98337a72a0
TheWire <TheWire@users.noreply.github.com>
2025-04-04 14:20:49 +0100

    start of chat functionality

---
+++ src/app/(main)/messages/Chat.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Loader2 } from "lucide-react";
+import useIntitializeChatClient from "./useInitializeChatClient";
+import { Chat as StreamChat } from "stream-chat-react";
+import ChatSidebar from "./ChatSidebar";
+import ChatChannel from "./ChatChannel";
+
+export default function Chat() {
+  const chatClient = useIntitializeChatClient();
+
+  if (!chatClient) {
+    return <Loader2 className="mx-auto my-3 animate-spin" />;
+  }
+
+  return (
+    <main className="relative w-full overflow-hidden rounded-2xl bg-card shadow-sm">
+      <div className="absolute bottom-0 top-0 flex w-full">
+        <StreamChat client={chatClient}>
+          <ChatSidebar />
+          <ChatChannel />
+        </StreamChat>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -0,0 +1,15 @@
+import { ChannelList } from "stream-chat-react";
+import { useSession } from "../sessionProvider";
+
+export default function ChatSidebar() {
+  const { user } = useSession();
+
+  return (
+    <div className="flex size-full flex-col border-e md:w-72">
+      <ChannelList
+        filters={{ type: "messaging", members: { $in: [user.id] } }}
+        showChannelSearch
+      />
+    </div>
+  );
+}
